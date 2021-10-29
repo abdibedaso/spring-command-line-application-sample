5f68dcc6ef4ca68e191171a5f6a2034887558531
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-29 01:54:06 -0500

    add staff rest controller spring web app

---
+++ room-web-app/src/main/java/com/geekcolab/development/roomwebapp/controllers/StaffRestController.java
@@ -0,0 +1,25 @@
+package com.geekcolab.development.roomwebapp.controllers;
+
+import java.util.List;
+
+import com.geekcolab.development.roomwebapp.models.StaffMember;
+import com.geekcolab.development.roomwebapp.service.StaffService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/staff")
+public class StaffRestController {
+
+    private final StaffService staffService;
+
+    public StaffRestController(StaffService staffService) {
+        this.staffService = staffService;
+    }
+
+    @GetMapping
+    public List<StaffMember> getAllStaff(){
+        return staffService.getAllStaff();
+    }
+}
